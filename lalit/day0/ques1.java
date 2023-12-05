890d2af7c293aa5f0ff3d79fada738a92d3b8dbb
Lalit Jangir <lalitkumarjangir02@gmail.com>
2023-12-05 20:13:31 +0530

    adding loop in ques1

--- lalit/day0/ques1.java
+++ lalit/day0/ques1.java
@@ -1,11 +1,13 @@
 //Rotate Array
 class Soluton{
     private void reverse(int nums[],int a,int b){
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
-        a++;
-        b--;
+        while(a<b){
+            int temp = nums[a];
+            nums[a] = nums[b];
+            nums[b] = temp;
+            a++;
+            b--;
+        }
     }
 
     public void rotate(int nums[],int k){
